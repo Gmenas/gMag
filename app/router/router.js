0630d57cc0ba1c66584ca61da3966d22a1331fff
Mark <mark.shark05@hotmail.com>
2017-07-11 19:35:37 +0300

    Add login, register, browse, error views

--- app/router/router.js
+++ app/router/router.js
@@ -1,9 +1,30 @@
 const attachTo = (app) => {
     app.get('/', (req, res) => {
-        const context = {
-            title: 'Home',
-        };
+        const context = { title: 'Home' };
         return res.render('home', context);
+    });
+
+    app.get('/browse', (req, res) => {
+        const context = { title: 'Browse products' };
+        return res.render('browse', context);
+    });
+
+    app.get('/login', (req, res) => {
+        const context = { title: 'Login' };
+        return res.render('login', context);
+    });
+
+    app.get('/register', (req, res) => {
+        const context = { title: 'Register' };
+        return res.render('register', context);
+    });
+
+    app.use((req, res) => {
+        const context = {
+            title: 'Not found',
+            url: req.url.slice(1),
+        };
+        return res.render('not-found', context);
     });
 };
 
