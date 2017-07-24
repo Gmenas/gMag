3fea497f5376071449ad39f86d751e07f0375f3b
dimitartalanov <d_talanov@yahoo.com>
2017-07-24 22:08:38 +0300

    istanbul and selenium config

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,43 @@ gulp.task('test:integration', () => {
     .pipe(istanbul.writeReports());
 });
 
+// for virtual server config
+const config = {
+  connectionStr: 'mongodb://localhost/gmag-test',
+  port: 3002,
+};
+gulp.task('server-start', () => {
+  return Promise.resolve()
+    .then(() => require('./db').init(config.connectionStr))
+    .then((db) => require('./data').init(db))
+    .then((data) => require('./data/initial.data').init(data))
+    .then((data) => require('./app').init(data, config.sessionSecret))
+    .then((app) => {
+      app.listen(config.port,
+        () => console.log(`Server running at port ${config.port}...`));
+    });
+});
+// remove virtual db
+const { MongoClient } = require('mongodb');
+gulp.task('server-stop', () => {
+  return MongoClient.connect(config.connectionStr)
+    .then((db) => {
+      return db.dropDatabase();
+    });
+});
+//
+
+gulp.task('test:browser', ['server-start'], () => {
+  return gulp.src('./test/browser/**/*.js')
+    .pipe(mocha({
+      reporter: 'nyan',
+      timeout: 10000,
+    }))
+    .once('end', () => {
+      gulp.start('server-stop');
+    });
+});
+
 gulp.task('pre-test', () => {
   return gulp.src([
     './app/**/*.js',
@@ -27,11 +64,18 @@ gulp.task('pre-test', () => {
     './db/**/*.js',
     './models/**/*.js',
     './server.js/**/*.js',
-  ]);
+  ])
+    // Covering files
+    .pipe(istanbul({
+      includeUntested: true,
+    }))
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', gulpsync.sync([
   'pre-test',
   'test:unit',
   'test:integration',
+  'test:browser',
 ]));
