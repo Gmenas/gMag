aaab0bfaa7ca13f978af5d196497be049d21bfd3
Ognyan <ogitaki@gmail.com>
2017-07-29 13:31:17 +0300

    Merge

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,25 +1,24 @@
-$(function () {
-  var skip = 9;
+$(function() {
+    var skip = 9;
 
-  $(window).on('scroll', function () {
-    var windowHeight = $(window).outerHeight();
-    var scrollTop = $(window).scrollTop();
-    var documentHeight = $(window).innerHeight();
+    $(window).on('scroll', function() {
+        var windowHeight = $(window).outerHeight();
+        var scrollTop = $(window).scrollTop();
+        var documentHeight = $(window).innerHeight();
 
-    if ((windowHeight + scrollTop) === documentHeight) {
-      var categoryUrl = window.location.pathname.split('/')[2];
-      var query = $.query.get();
+        if ((windowHeight + scrollTop) === documentHeight) {
+            var categoryUrl = window.location.pathname.split('/')[2];
+            var query = $.query.get();
 
-      $.get('/api/products', {
-        categoryUrl: categoryUrl,
-        q: query.q,
-        p: query.p,
-        skip: skip,
-      }, function (products) {
-        $('.row.scroll').append(products);
-        // skip += skip;-taka nqma da zaredi ako ima ostavashti kolkoto i da sa po malko ot 9 obqvi naprimer ima oshte 3 a skipvame 9
-        skip = $('.text-muted').length;// wzimam kolkoto ima do momenta tolkova da skipne moje da sa samo 3 a ne 9 naprimer :)
-      });
-    }
-  });
+            $.get('/api/products', {
+                categoryUrl: categoryUrl,
+                q: query.q,
+                p: query.p,
+                skip: skip,
+            }, function(products) {
+                $('.row.scroll').append(products);
+                skip += skip;
+            });
+        }
+    });
 });
