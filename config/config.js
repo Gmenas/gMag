3ba690e50d0c2cd4741cdf0c1db550e8213d1977
Mark <mark.shark05@hotmail.com>
2017-07-30 13:47:14 +0300

    Add production config file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,18 @@
 /* globals __dirname */
 const path = require('path');
+const fs = require('fs');
+
+const configFilePath = './production.config.json';
+const initialDataFilePath = './init.data.json';
+
+let config = {};
+if (fs.existsSync(configFilePath)) {
+    config = JSON.parse(fs.readFileSync(configFilePath));
+}
 
 module.exports = {
-    port: 80,
-    connectionStr: 'mongodb://localhost/gmag',
-    sessionSecret: 'magic spoon',
-    initialDataFile: path.join(__dirname, './init.data.json'),
+    port: config.port || 80,
+    connectionStr: config.connectionStr || 'mongodb://localhost/gmag',
+    sessionSecret: config.sessionSecret || 'magic spoon',
+    initialDataFile: path.join(__dirname, initialDataFilePath),
 };
