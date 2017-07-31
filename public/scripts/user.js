f86925ba8d7e2af19030dee2f31ae4362e6737b7
Mark <mark.shark05@hotmail.com>
2017-07-31 22:30:57 +0300

    Fix bugs with user tabs

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,6 +1,5 @@
 $(function() {
     var skipStep = 9;
-    var activeTab = '#products';
     var products = {
         filter: window.ctx.productFilter,
         skip: skipStep,
@@ -9,9 +8,13 @@ $(function() {
         filter: window.ctx.favFilter,
         skip: skipStep,
     };
+    var activeTab = window.location.hash || '#products'
+
+    $('.nav-tabs a[href="'+activeTab+'"]').tab('show');
 
     $('.nav-tabs a').on('shown.bs.tab', function(event) {
         activeTab = $(event.target).attr('href');
+        window.location.hash = activeTab;
     });
 
     $(window).on('scroll', function() {
